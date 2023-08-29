d288094d344e37357951927822a290aee78d52e2
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-08-30 00:15:12 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { Body } = require("node-fetch");
+
 // Add your code here
+function submitData(name, email) {
+    const user = { name, email };
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(user)
+
+    })
+        .then((res) => res.json())
+        .then(data => document.body.innerHTML += `<p>id = ${data.id}</p>`)
+        .catch(error => document.body.innerHTML += `<p>id = ${error.message}</p>`)
+
+}
